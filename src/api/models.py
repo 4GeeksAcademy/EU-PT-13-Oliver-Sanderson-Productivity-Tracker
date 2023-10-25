a3052ac302bac52f6ab2e23fb0d3c9abb5b6fd1f
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2023-10-25 11:55:48 +0000

    started backend

--- src/api/models.py
+++ src/api/models.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime
+import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
@@ -15,5 +18,43 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
             # do not serialize the password, its a security breach
+        }
+    
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date = db.Column(db.DateTime, nullable=False)
+    time_spent_secs = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Session {self.date}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "date": self.date,
+            "time_spent_secs": self.time_spent_secs,
+        }
+    
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    test_value = db.Column(db.String(120), nullable=False)
+
+    Obj3 = datetime.datetime.now()
+    todayDate = Obj3.date()
+    todayTime = Obj3.time()
+    print(Obj3)
+    print(todayDate)
+    print(todayTime)
+
+    def __repr__(self):
+        return f'<Test {self.test_value}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "test_value": self.test_value,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,70 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Session, Test
 from api.utils import generate_sitemap, APIException
+import datetime
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+
+
+@api.route('/users', methods=['GET'])
+def handle_users():
+
+    users = User.query.all()
+
+    response_body = {}
+
+    for index, user in enumerate(users):
+        response_body["id" + str(index)] = (user.id)
+
+
+    return jsonify(response_body), 200
+
+@api.route('/sessions', methods=['GET'])
+def handle_sessions():
+
+    # We create an instance without being recorded in the database
+    session = Session()
+    session.user_id = 3
+    session.email = "test@test.com"
+    session.date = datetime.datetime.now()
+    session.time_spent_secs = 666
+
+
+    # We tell the database we want to record this user
+    db.session.add(session)
+    db.session.commit()
+
+    sessions = Session.query.all()
+
+    response_body = {}
+
+    for index, session in enumerate(sessions):
+        response_body["date" + str(index)] = (session.date)
+
+    return jsonify(response_body), 200
+
+@api.route('/test', methods=['GET'])
+def handle_tests():
+
+    # We create an instance without being recorded in the database
+    testA = Test()
+    testA.test_value = "Sausage"
+
+    # We tell the database we want to record this user
+    db.session.add(testA)
+    db.session.commit()
+
+    tests = Test.query.all()
+
+    response_body = {}
+
+    for index, testers in enumerate(tests):
+        response_body["value" + str(index)] = (testers.test_value)
 
     return jsonify(response_body), 200
